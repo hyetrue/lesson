84242066455c53bde328ad04595b565326509443
hyetrue <appleehj@gmail.com>
2023-04-02 14:50:56 +0900

    수정

--- Test.js
+++ Test.js
@@ -1,11 +1,7 @@
 const Test = () => {
-<<<<<<< HEAD
   return (
     <div>
       테스트하는 중<p>충돌테스트</p>
     </div>
   );
-=======
-  return <div>테스트하는 중</div>;
->>>>>>> parent of 0f383e9 (Test수정)
 };
